161f470f34300dda52408a44aaebe3aecd3cca47
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-02-05 16:39:11 +0200

    take input and validate data

--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -17,28 +17,51 @@ class AddNoteBottomSheet extends StatelessWidget {
   }
 }
 
-class AddNoteForm extends StatelessWidget {
+class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
     super.key,
   });
 
   @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subTitle;
+
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
         children: [
-          SizedBox(height: 32),
+          const SizedBox(height: 32),
           CustomTextField(
             hintText: 'Title',
+            onSaved: (value) => title = value,
           ),
-          SizedBox(height: 16),
+          const SizedBox(height: 16),
           CustomTextField(
             hintText: 'Content',
+            onSaved: (value) => subTitle = value,
             maxLines: 5,
           ),
-          SizedBox(height: 16),
-          CustomButton(),
-          SizedBox(height: 16),
+          const SizedBox(height: 16),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(height: 16),
         ],
       ),
     );
